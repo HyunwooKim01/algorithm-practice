1d7c60136e7f3d55b82d1ca3a04fc0459d80407c
HyunwooKim01 <126743699+HyunwooKim01@users.noreply.github.com>
2023-05-30 00:12:09 +0900

    Create bubble.py

---
+++ sort/basic/bubble.py
@@ -0,0 +1,13 @@
+def bubbleSort(A):
+    for numElements in range(len(A), 0, -1):
+        for i in range(numElements-1):
+            if A[i] > A[i+1]:
+                A[i], A[i+1] = A[i+1], A[i]
+
+def bubbleSortRec(A, n): 
+    for i in range(n-1):  
+        if A[i] > A[i+1]:  
+            A[i], A[i+1] = A[i+1], A[i]  
+
+    if n > 1: 
+        bubbleSortRec(A, n-1)
